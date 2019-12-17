fe0ca8ee948b9147b1f864c0b5f2ec26fe474186
Jim <jimyyang129@gmail.com>
2019-12-16 19:01:05 -0800

    Fininshed Lab 7 and add toast with random number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastService } from './toast/toast.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'toast-app';
+  toastTypes: Array<string> = [];
+
+
+  constructor(private toastService: ToastService) {
+    this.toastTypes = ['success', 'warning', 'danger', 'info'];
+  }
+  // success, warning, danger, info
+
+  showToast() {
+    const rand = Math.floor(Math.random() * 4);
+    console.log('random number...', rand);
+    const toastType = this.toastTypes[rand];
+    const toastMessage = 'Hi, my name is Jim Yang and the random number for this is a message: ' + rand;
+    const duration = 3000;
+
+    this.toastService.showToast(toastType, toastMessage, duration);
+  }
 }
